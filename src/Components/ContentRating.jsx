9a10e3da82c2262d1f0ef7141b4e1ef1e49b6f25
MasatoSotoma <masatosotoma@gmail.com>
2024-10-22 19:18:34 -0400

    updated the content

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,16 +1,19 @@
-
-import React, { Component } from 'react';
-import './ContentRating.css';
+import React, { Component } from "react";
+import "./ContentRating.css";
 
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+    };
   }
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      <>
+        <div className="content-rating"></div>
+      </>
     );
   }
 }
